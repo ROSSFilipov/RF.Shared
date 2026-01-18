f9cc3cd71fe1271c534acefd72d76a2aaeff090c
Ruslan Filipov <rossfilipov@gmail.com>
2026-01-18 13:21:20 +0200

    Result pattern implementation
    * Result pattern implementation
    
    * Result pattern implementation
    
    * Result pattern implementation
    
    * Result pattern implementation

---
+++ src/RF.Shared.Common/Models/V1/Error.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Frozen;
+
+namespace RF.Shared.Common.Models.V1;
+
+public sealed record Error
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Error"/> class with the specified title, code, message, and optional extensions.
+    /// </summary>
+    /// <param name="title">The short, human-readable summary of the error.</param>
+    /// <param name="code">The numeric code that uniquely identifies the error.</param>
+    /// <param name="message">The detailed message describing the error.</param>
+    /// <param name="extensions">An optional dictionary containing additional key-value pairs that provide extra context about the error.</param>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="title"/> or <paramref name="message"/> is null, empty, or consists only of white-space characters.</exception>"
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="extensions"/> is null.</exception>"
+    public Error(string title, int code, string message, IDictionary<string, string> extensions = null)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(title);
+        ArgumentException.ThrowIfNullOrWhiteSpace(message);
+        ArgumentOutOfRangeException.ThrowIfNegative(code);
+
+        Title = title;
+        Code = code;
+        Message = message;
+        Extensions = extensions?.ToFrozenDictionary()
+            ?? FrozenDictionary<string, string>.Empty;
+    }
+
+    public string Title { get; }
+
+    public int Code { get; }
+
+    public string Message { get; }
+
+    public FrozenDictionary<string, string> Extensions { get; }
+
+    public bool Equals(Error other)
+    {
+        return Title == other.Title
+               && Code == other.Code
+               && Message == other.Message
+               && Extensions.Count == other.Extensions.Count
+               && !Extensions.Except(other.Extensions).Any();
+    }
+
+    public override int GetHashCode()
+    {
+        var hash = new HashCode();
+        hash.Add(Title);
+        hash.Add(Code);
+        hash.Add(Message);
+        return hash.ToHashCode();
+    }
+}
---
+++ src/RF.Shared.Common/Models/V1/Result.cs
@@ -0,0 +1,74 @@
+﻿namespace RF.Shared.Common.Models.V1;
+
+/// <summary>
+/// Represents the outcome of an operation, indicating success or failure and providing error information if applicable.
+/// </summary>
+/// <remarks>Use the <see cref="Result"/> class to standardize the representation of operation results, especially
+/// in scenarios where an operation can fail and you want to avoid using exceptions for control flow. A successful
+/// result is created using <see cref="Success"/>, while a failed result is created using <see cref="Failure(Error)"/>
+/// or by implicitly converting an <see cref="Error"/> to a <see cref="Result"/>. The <see cref="IsSuccess"/> property
+/// indicates whether the operation succeeded, and the <see cref="Error"/> property provides details about the failure
+/// if any. Implicit conversions allow you to return an error directly from a method without using the Result factory methods.
+/// </remarks>
+public class Result
+{
+    protected Result()
+    {
+        IsSuccess = true;
+    }
+
+    protected Result(Error error)
+    {
+        ArgumentNullException.ThrowIfNull(error);
+
+        Error = error;
+        IsSuccess = false;
+    }
+
+    public bool IsSuccess { get; }
+
+    public Error Error { get; }
+
+    public static Result Success() => new();
+
+    public static Result Failure(Error error) => new(error);
+
+
+    public static implicit operator Result(Error error) => Failure(error);
+}
+
+/// <summary>
+/// Represents the outcome of an operation that returns a value, encapsulating either a successful result with a value
+/// of the specified type or a failure with an associated error.
+/// </summary>
+/// <remarks>Use this type to indicate the success or failure of an operation while carrying a value in the
+/// success case or an error in the failure case. The <see cref="Result{T}"/> type provides static methods and implicit
+/// conversions to simplify creating success or failure results. Accessing the <see cref="Value"/> property is only
+/// valid when the result represents success; otherwise, it may throw an exception or yield undefined
+/// behavior. Implicit conversions allow you to return a value of the generic type or an error directly from a method 
+/// without using the Result factory methods.</remarks>
+/// <typeparam name="T">The type of the value returned on a successful result.</typeparam>
+public sealed class Result<T> : Result
+{
+    private Result(T value)
+    {
+        if (value is null)
+        {
+            throw new ArgumentNullException(nameof(value));
+        }
+
+        Value = value;
+    }
+
+    private Result(Error error) : base(error) { }
+
+    public T Value { get; }
+
+    public static Result<T> Success(T value) => new(value);
+
+    public static new Result<T> Failure(Error error) => new(error);
+
+    public static implicit operator Result<T>(T value) => Success(value);
+
+    public static implicit operator Result<T>(Error error) => Failure(error);
+}
